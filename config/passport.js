0794fe2edaf404aabe8a6893b0b2bed8676744ff
Purushotam-Thakur <purushotam261850@gmail.com>
2018-08-29 23:13:20 +0530

    small changes

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const JwtStrategy = require('passport-jet').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
@@ -10,10 +10,10 @@ opts.secreatOrKey = keys.secretOrKey;
 
 module.exports = passport => {
 	passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-			//console.log(jwt_payload);
-			User.findById(jwt_payload.id)
+		//console.log(jwt_payload);
+		User.findById(jwt_payload.id)
 			.then(user => {
-				if(user){
+				if (user) {
 					return done(null, user)
 				}
 				return done(null, false);
