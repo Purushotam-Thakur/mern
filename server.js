a3cc712961ff625ae1fd366a3553921479b49bae
Purushotam-Thakur <purushotam261850@gmail.com>
2018-09-03 02:01:59 +0530

    implementing React

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/', (req, res) => res.send('Hello World'));
 //Use Routes
 app.use('/api/users', usersRouter);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server runing on port : ${port}`));
