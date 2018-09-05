5e5f77bf4ef0502334bde780d709aeda3a9b2300
Purushotam Thakur <purushotam261850@gmail.com>
2018-09-05 12:57:00 +0530

    formating

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -4,7 +4,7 @@ import { GET_ERRORS } from './types';
 // Register User
 export const registerUser = (userData, history) => dispatch => {
      axios.post('/api/users/register', userData)
-             .then(res => history.push('/login)
+             .then(res => history.push('/login')
              .catch(err => 
                    dispatch({
                         type : GET_ERRORS,
