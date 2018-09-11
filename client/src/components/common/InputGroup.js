b32b520b202bc5b8e81ab0ed1f0f5b56aa54a873
Purushotam Thakur <purushotam261850@gmail.com>
2018-09-11 11:39:21 +0530

    Form Field Components

---
+++ client/src/components/common/InputGroup.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+const InputGroup = ({
+  name,
+  placeholder,
+  value,
+  error,
+  icon,
+  type,
+  onChange,
+}) => {
+  return (
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <span className="Input-group-text">
+          <i className(icon)>
+        </span>
+      </div>
+      <input
+        className={classnames('form-control form-control-lg', { 'is-invalid': error })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+      {error && (<div className='invalid-feedback'>{error}</div>)}
+    </div>
+  )
+}
+
+InputGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  info: PropTypes.string,
+  icon: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+}
+
+InputGroup.defaultProps = {
+  type: test
+}
+
+export default InputGroup;
