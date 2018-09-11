f93e4886ba3b2e1d4cbf57090b9daec2d28d6df9
Purushotam Thakur <purushotam261850@gmail.com>
2018-09-11 11:39:18 +0530

    Form Field Components

---
+++ client/src/components/common/SelectListGroup.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+const SelectListGroup = ({
+  name,
+  value,
+  error,
+  info,
+  onChange,
+  options
+}) => {
+  const selectOprions = options.map(option => (
+    <option key={option.label} value={option.value}>
+      {option.label}
+    </options
+  ))
+
+  return (
+    <div className="form-group">
+      <select
+        className={classnames('form-control form-control-lg', { 'is-invalid': error })}
+        name={name}
+        value={value}
+        onChange={onChange}>
+        {selectOption}
+      </select>
+      {info && <small className="form-text text-muted">{info}</small>}
+      {error && (<div className='invalid-feedback'>{error}</div>)}
+    </div>
+  )
+}
+
+SelectListGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  info: PropTypes.string,
+  error: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired
+}
+
+export default SelectListGroup;
