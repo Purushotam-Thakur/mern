680fe9fd70db92b33142609fcf542647083611d4
Purushotam Thakur <purushotam261850@gmail.com>
2018-09-18 12:34:59 +0530

    Dashboard Experience Display Delete

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -34,7 +34,7 @@ export const createProfile= (profileData, history) => dispatch => {
 }
 
 // Add experience
-export const addExperience = { expData, history} => dispatch => {
+export const addExperience = (expData, history) => dispatch => {
   axios
     .post('/api/profile/experience', expData)
     .then(res => history.push('/dashboard'))
@@ -47,10 +47,28 @@ export const addExperience = { expData, history} => dispatch => {
 }
 
 // Add education
-export const addEducation = { eduData, history} => dispatch => {
+export const addEducation = (eduData, history) => dispatch => {
   axios
     .post('/api/profile/education', eduData)
     .then(res => history.push('/dashboard'))
+    .catch(err => 
+          dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+          })
+    );
+}
+
+// Delete Experience
+export const deleteExperience = ( id ) => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`, eduData)
+    .then(res => 
+         dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+          })
+         )
     .catch(err => 
           dispatch({
             type: GET_ERRORS,
